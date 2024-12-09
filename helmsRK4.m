b66cd488814246ac99e5fe6f427d08d20b34bd53
alcampbell30 <2572664c@student.gla.ac.uk>
2024-12-09 19:36:14 +0000

    Convergence and Parameter Scaling
    Code to calculate numerical error was adapted from class material and added.
    Parameters were scaled suitably according to their units, e.g. heat production values were scaled by e^-6 to reflect the information on the excel data sheet. Sigma had to be scaled by *1000 since the units were per unit length rather than unit volume. 
    A line for the analytical solution was added to use for the numerical error.

--- helmsRK4.m
+++ helmsRK4.m
@@ -14,12 +14,13 @@ zf = 0:h:D; % z-coordinate vectore for cell face positions [m]
 %Insulating Sides
 ix3 = [ 1,1:Nx,Nx ];
 
-
 %Set up Insulating Top and Bottom
 iz3 = [ 1,1:Nz,Nz ];
 
+
+
 % set initial condition for temperature at cell centres
-T = Ttop + geotherm.*Zc; % initialise T array on linear gradient
+T = Ttop + geotherm(2).*Zc; % initialise T array on linear gradient
 T(air) = Ttop;
 % initialise density and mobility
 rho = rho0.*(1 - aT.*(T-Ttop));
@@ -27,10 +28,11 @@ kT = kT0.*ones(Nz,Nx);
 
 % initialise output figure with initial condition
 figure(1); clf
-makefig(xc,zc,T,0,yr)
+makefig(xc,zc,T)
 
 %***** Solve Model Equations
 dt = CFL * (h/2)^2/max(kT(:)); % initial time step [s]
+
 t = 0; % initial time [s]
 k = 0; % initial time step count
 dTdt = 0;
@@ -52,25 +54,39 @@ while t <= tend
     dTdt3 = diffusion(T+dTdt2/2*dt,kT, h,ix3,iz3,geotherm, Hr, rho, Cp);
     dTdt4 = diffusion(T+dTdt3  *dt,kT, h,ix3,iz3,geotherm, Hr, rho, Cp);
 
-    Hs = Hr ./(rho.*Cp);
+    Hs = (Hr*10^-6)./(rho.*Cp); %scale Hr to units of Watts 
 
-    T = T + (dTdt1 + 2*dTdt2 + 2*dTdt3 + dTdt4)/6 * dt +Hs;
+    T = T + (dTdt1 + 2*dTdt2 + 2*dTdt3 + dTdt4)/6 * dt + Hs;
 
     T(air)=Ttop;
-   
+
+    Tana= T + geotherm(2).*Zc;
+     
 
             % plot model progress every 'nop' time steps
         if ~mod(k,nop)
-            makefig(xc,zc,T,t,yr);
+            makefig(xc,zc,T);
         end
+
 
 end
 
+        
+%*****  calculate numerical error norm
 
+Errx = norm(T - Tana,2)./norm(Tana,2); %error in x
+Errz = norm(T - Tana,1)./norm(Tana,1); %error in z
+
+
+disp(' ');
+disp('Time integration scheme: RK4')
+disp(['Numerical error in x = ',num2str(Errx)]);
+disp(['Numerical error in z = ',num2str(Errz)]);
+disp(' ');
 %***** Utility Functions ************************************************
 
 % Function to make output figure
-function makefig(x,z,T,t,yr)
+function makefig(x,z,T)
 
     % plot temperature in subplot 1
     imagesc(x,z,T); axis equal; c=colorbar; hold on
@@ -78,12 +94,15 @@ function makefig(x,z,T,t,yr)
 
     [C, h] = contour(x, z, T, [150, 150], 'r', 'LineWidth', 2); % 150°C contour in red
     clabel(C, h, 'FontSize', 12, 'Color', 'r'); % Optional: Label the 150°C contour
+
+    [C, h] = contour(x, z, T, [100, 100], 'r', 'LineWidth', 2); % 100°C contour in red
+    clabel(C, h, 'FontSize', 12, 'Color', 'r'); % Optional: Label the 100°C contour
     drawnow
      
     xlabel('Horizontal Distance [m]', 'FontSize',15)
     ylabel('Depth [m]','FontSize',15)
     ylabel(c, 'C', 'FontSize',15)
-    title(['Temperature [C]; time = ',num2str(t/yr),' yr'],'FontSize',18)
+    title('Temperature','FontSize',18)
 
 end
 
@@ -97,7 +116,7 @@ function [dTdt] = diffusion(f,k,h,ix,iz,geotherm, Hr, rho, Cp)
     qz = - kz .* diff(f(iz,:), 1, 1)/h; %Calculate heat flux across the cell boundaries using kz in the z direction 
 
     % basalt boundary
-    qz(end,:) = - kz(end,:) .* geotherm;
+    qz(end,:) = - kz(end,:) .* geotherm(2);
 
     % calculate flux balance for rate of change
     dTdt_diffusion = - (diff(qx,1,2)/h+diff(qz,1,1)/h);
@@ -108,3 +127,4 @@ function [dTdt] = diffusion(f,k,h,ix,iz,geotherm, Hr, rho, Cp)
     % total rate of change of temperature
     dTdt = dTdt_diffusion + heat_source;
 end
+
